ea3627b2cfb0943e1e1d1007b64afaea47bc636e
Arwin Sondhi <arwin.sondhi@mail.utoronto.ca>
2023-10-26 18:10:14 -0400

    flask local and postgresql contaier communication implemented

--- backend/info_retrieval/hello.py
+++ backend/info_retrieval/hello.py
@@ -1,7 +1,47 @@
+# https://flask.palletsprojects.com/en/3.0.x/quickstart/
+# flask --app hello run (source venv first)
+
 from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
 
-@app.route("/")
+
+# Configure the PostgreSQL connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:root@localhost:6543/test_db'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+@app.get("/")
+def index():
+    return "<p>Index Page</p>"
+
+@app.get("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+class DummyTable(db.Model):
+    __tablename__ = 'dummy_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+
+@app.get("/db_conn")
+def db_connect():
+    try:
+        results = DummyTable.query.order_by(DummyTable.id.asc()).all()
+        
+        # Convert the results to a list of dictionaries
+        results_list = [{"id": result.id, "name": result.name, "age": result.age} for result in results]
+        
+        return jsonify(results_list), 200
+
+        # db.session.query(text("1")).from_statement(text("SELECT 1")).all()
+        # return jsonify({"message": "Connection successful!"}), 200
+    
+    except Exception as e:
+        return jsonify({"message": "Connection failed!", "error": str(e)}), 500
